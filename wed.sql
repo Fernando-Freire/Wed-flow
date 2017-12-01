df1b56837873248947bc45e820c36b62f5b4641f
Fernando Freire Scattone <fernando.scattone@usp.br>
2017-12-01 11:23:56 -0200

    Update wed.sql

--- wed.sql
+++ wed.sql
@@ -6,7 +6,7 @@ begin ;
 CREATE wed-attribute Usuário_ID, Usuário_Status, Conta_ID, Conta_status,Data_Última_Assinatura, Pagamento_status, Lista_ID, Lista_status;
 
 
-CREATE Wed-condition Contrata as Pagamentos_status = pago;
+CREATE Wed-condition Contrata as Pagamentos_status = pago and Usuário_Tipo = 'Titular';
 
 CREATE Wed-condition Anula_contrato as Data_Última_Assinatura < (Today - 1 year);
 
@@ -14,7 +14,7 @@ CREATE Wed-condition Listas_Remover as Conta_status = 'grátis' and Lista_ID not
 
 CREATE Wed-condition Usuário_removido as Usuário_Tipo = 'comum' and Usuário_Status = 'Removido';
 
-CREATE Wed-condition Usuário_Titular_Removido as Usuário_Tipo = 'Titular' and Usuário_Status = 'Removido';
+CREATE Wed-condition Usuário_Titular_removido as Usuário_Tipo = 'Titular' and Usuário_Status = 'Removido';
 
 
 
@@ -38,9 +38,13 @@ CREATE Wed-trigger C (Listas_Remover,Remover_Listas);
 
 CREATE Wed-trigger D (Usuário_removido,Remover_Usuário);
 
-CREATE Wed-trigger E (Usuário_Titular_Removido,Remover_Usuário);
+CREATE Wed-trigger E (Usuário_removido,Remover_Listas);
 
-CREATE Wed-trigger F (Usuário_Titular_Removido,Remover_Conta);
+CREATE Wed-trigger F (Usuário_Titular_removido,Remover_Usuário);
+
+CREATE Wed-trigger G (Usuário_Titular_removido,Remover_Listas);
+
+CREATE Wed-trigger H (Usuário_Titular_removido,Remover_Conta);
 
 
 
