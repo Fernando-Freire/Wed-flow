29c45bd75080b166df76bf4571b15c60264e9479
Fernando Freire Scattone <fernando.scattone@usp.br>
2017-12-01 11:05:02 -0200

    Create wed.sql

---
+++ wed.sql
@@ -0,0 +1,50 @@
+CREATE flow dismus:
+\cw dismus
+
+begin ;
+
+CREATE wed-attribute Usuário_ID, Usuário_Status, Conta_ID, Conta_status,Data_Última_Assinatura, Pagamento_status, Lista_ID, Lista_status;
+
+
+CREATE Wed-condition Contrata as Pagamentos_status = pago;
+
+CREATE Wed-condition Anula_contrato as Data_Última_Assinatura < (Today - 1 year);
+
+CREATE Wed-condition Listas_Remover as Conta_status = 'grátis' and Lista_ID not null;
+
+CREATE Wed-condition Usuário_removido as Usuário_Tipo = 'comum' and Usuário_Status = 'Removido';
+
+CREATE Wed-condition Usuário_Titular_Removido as Usuário_Tipo = 'Titular' and Usuário_Status = 'Removido';
+
+
+
+CREATE Wed-transition Contrato_anulado;
+
+CREATE Wed-transition Remover_Listas;
+
+CREATE Wed-transition Assinatura_Contratada;
+
+CREATE Wed-transition Remover_Usuário;
+
+CREATE Wed-transition Remover_Conta;
+
+
+
+CREATE Wed-trigger A (Contrata,Assinatura_Contratada);
+
+CREATE Wed-trigger B (Anula_contrato,Contrato_anulado);
+
+CREATE Wed-trigger C (Listas_Remover,Remover_Listas);
+
+CREATE Wed-trigger D (Usuário_removido,Remover_Usuário);
+
+CREATE Wed-trigger E (Usuário_Titular_Removido,Remover_Usuário);
+
+CREATE Wed-trigger F (Usuário_Titular_Removido,Remover_Conta);
+
+
+
+Set final wed-state as Usuário_Tipo = 'Titular' and Usuário_Status = 'Removido' and Conta_Status = 'Removida' and Lista_Status = 'Removida';
+
+
+commit;
